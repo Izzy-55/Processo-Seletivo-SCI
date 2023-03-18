27010aebc6f31f58d7e947d9ff6ff167c32a1baa
Izzy-55 <122451438+Izzy-55@users.noreply.github.com>
2023-03-18 14:17:53 -0300

    Update 3Python.py

--- SCI/3Python.py
+++ SCI/3Python.py
@@ -1,7 +1,8 @@
 # Elabore um programa de computador que realize o cálculo de notas.
 # Este programa deverá solicitar o nome do aluno e quatro notas.
 # Todo este conjunto deverá estar contido em um laço que confirme se deseja encerrar o programa ou continuar solicitando outros nomes e notas.
-# Ao final da solicitação do nome e das notas deverá ser impresso o nome do aluno e a sua média, se a nota for  menor que 6 imprimir Reprovado, senão, se a nota for igual ou maior que 6, imprimir aprovado.
+# Ao final da solicitação do nome e das notas deverá ser impresso o nome do aluno e a sua média, se a nota for  menor que 6 imprimir Reprovado, 
+# senão, se a nota for igual ou maior que 6, imprimir aprovado.
 
 quebrar_loop = False
 
